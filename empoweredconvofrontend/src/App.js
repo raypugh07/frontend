f2a3d6214bd6f69ed7b2fa996c0e087ecc80cf45
Nolan Allen <nolan.r.allen@gmail.com>
2019-08-06 11:26:49 -0500

    2nd commit: removed forms files from fe-navfooter branch; general branch cleanup

--- empoweredconvofrontend/src/App.js
+++ empoweredconvofrontend/src/App.js
@@ -4,10 +4,7 @@ import { login, register } from './actions/authActions';
 import { getUsers } from './actions/getUsers';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import UsersList from './components/UsersList/UsersList';
-import { Header } from 'semantic-ui-react';
 import Bar from './components/NavBar/Bar';
-import FormView from './components/Form/FormView';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
@@ -23,10 +20,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          {/* <Header as="h1">Empowered Conversations</Header> */}
-          {/* <UsersList /> */}
           <Bar />
-          <FormView />
           <Footer />
         </div>
       </Router>
