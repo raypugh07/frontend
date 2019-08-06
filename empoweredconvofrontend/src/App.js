e9a12431d8534e3eeb84e23814ae81796fb59540
Your Name <you@example.com>
2019-08-06 13:07:27 -0500

    removed non about files,about.js

--- empoweredconvofrontend/src/App.js
+++ empoweredconvofrontend/src/App.js
@@ -4,10 +4,11 @@ import { login, register } from './actions/authActions';
 import { getUsers } from './actions/getUsers';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-import UsersList from './components/UsersList/UsersList';
+/*import UsersList from './components/UsersList/UsersList';
 import { Header } from 'semantic-ui-react';
 import Bar from './components/NavBar/Bar';
-import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';*/
+import About from './components/About/About';
 
 class App extends Component {
   state = {
@@ -36,10 +37,11 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Bar/>
-          <Header as="h1">Empowered Conversations</Header>
-          <UsersList />
-          <Footer/>
+        
+        
+          <Route path='/about' component={About}/>
+
+         
         </div>
       </Router>
     );
--- empoweredconvofrontend/src/components/About/About.js
+++ empoweredconvofrontend/src/components/About/About.js
@@ -1,11 +1,19 @@
 import React from 'react'
+//import { NavHeader, Nav, LogoDiv, NavBarLogo, linkDec, navButton,MobNav } from '../../styles/NavStyling';
+import './About.css'
 
 
 
 export default function About(){
     return(
-        <div>
-            
+        <div className='about-page'>
+            <div className='about-text'>
+            <p>Who We Are</p>
+            <p>A survivor-activated tool that educates and prepares the person they will disclose to before the conversation takes place.</p>
+            <p>Our tool takes the burden off of the survivor for having to educate their loved ones about assault during disclosure because we provide that training ahead of time.</p>
+            <button className='aboutbtn' >Use The Tool</button>
+            </div>
+
         </div>
     )
 }
